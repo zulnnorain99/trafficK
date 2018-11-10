868a5169ca230c0ee199b55be4c6daaaa3403345
AlvaroRausell <alvaro.rausellg@gmail.com>
2018-11-10 17:54:14 +0000

    Add stations dictionary

--- www/data.js
+++ www/data.js
@@ -6,7 +6,6 @@ var clapham_junction_main_coord = new L.LatLng(51.4636882,  -0.1712424);
 var vauxhall_main_coord = new L.LatLng(51.486183, -0.122860);
 var wimbledon_coord = new L.LatLng(51.4214158, -0.2075719);
 var surbition_coord = new L.LatLng(51.3947441, -0.3063072);
-
 var liphook_coord = new L.LatLng(51.071556, -0.800071);
 var walton_on_thames_coord = new L.LatLng(51.372999, -0.414313);
 var rowlands_castle_coord = new L.LatLng(50.892427, -0.957471);
@@ -16,7 +15,7 @@ var hilsea_coord = new L.LatLng(50.828442, -1.058773);
 var guildford_coord = new L.LatLng(51.236873, -0.580211);
 var bedhampton_coord = new L.LatLng(50.854024, -0.996437);
 var worplesdon_coord = new L.LatLng(51.288205, -0.582492);
-var woking = new L.LatLng(51.318505, -0.557043);
+var woking_coord = new L.LatLng(51.318505, -0.557043);
 var byfleet_and_new_haw_coord = new L.LatLng(51.349983, -0.481260);
 var portsmouth_and_southsea_coord = new L.LatLng(50.798584, -1.090677);
 var havant_coord = new L.LatLng(50.854378, -0.981613);
@@ -35,18 +34,46 @@ var farncombe_coord = new L.LatLng(51.197198, -0.604523);
 
 
 
-{'LIPHOOK', 'WALTON ON THAMES', 'ROWLANDS CASTLE', 'PETERSFIELD', 'WIMBLEDON (WESSEX SIDE)', 'EARLSFIELD', 'HILSEA', 'GUILDFORD', 'BEDHAMPTON', 'WORPLESDON',
- 'LONDON WATERLOO', 'WOKING', 'BYFLEET & NEW HAW', 'PORTSMOUTH & SOUTHSEA', 'HAVANT', 'GODALMING', 'FRATTON', 'VAUXHALL (MAIN)', 'HERSHAM', 'HASLEMERE',
- 'WEST BYFLEET', 'MILFORD', 'CLAPHAM JUNCTION MAIN (9-11)', 'SURBITON', 'PORTSMOUTH HARBOUR', 'WITLEY', 'ESHER', 'LISS', 'WEYBRIDGE', 'FARNCOMBE'}
+var stations = {'LIPHOOK':liphook_coord,
+                'WALTON ON THAMES':walton_on_thames_coord,
+                'ROWLANDS CASTLE':rowlands_castle_coord,
+                'PETERSFIELD':petersfield_coord,
+                'WIMBLEDON (WESSEX SIDE)':wimbledon_coord,
+                'EARLSFIELD':earlsfield_coord,
+                'HILSEA':hilsea_coord,
+                'GUILDFORD':guildford_coord,
+                'BEDHAMPTON':bedhampton_coord,
+                'WORPLESDON':worplesdon_coord,
+                'LONDON WATERLOO':waterloo_london_coord,
+                'WOKING':woking_coord,
+                'BYFLEET & NEW HAW':byfleet_and_new_haw_coord,
+                'PORTSMOUTH & SOUTHSEA':portsmouth_and_southsea_coord,
+                'HAVANT':havant_coord,
+                'GODALMING':godalming_coord,
+                'FRATTON':fratton_coord,
+                'VAUXHALL (MAIN)':vauxhall_main_coord,
+                'HERSHAM':hersham_coord,
+                'HASLEMERE':haslemere_coord,
+                'WEST BYFLEET':west_byfleet_coord,
+                'MILFORD':milford_coord,
+                'CLAPHAM JUNCTION MAIN (9-11)':clapham_junction_main_coord,
+                'SURBITON':surbition_coord,
+                'PORTSMOUTH HARBOUR':portsmouth_harbour_coord,
+                'WITLEY':witley_coord,
+                'ESHER':esher_coord,
+                'LISS':liss_coord,
+                'WEYBRIDGE':weybridge_coord,
+                'FARNCOMBE':farncombe_coord};
 
-var waterloo_london = [waterloo_london_coord, "Waterloo London", 100];
-var clapham_junction_london = [clapham_junction_london_coord, "Clapham Junction London",50];
-var vauxhall_london = [vauxhall_london_coord, "Vauxhall London",100];
-var wimbledon = [wimbledon_coord, "Wimbledon",10];
-var surbition = [surbition_coord, "Surbition",79];
-var richmond = [richmond_coord, "Richmond",0];
-var raynes_park = [raynes_park_coord, "Raynes Park",35];
-var earlsfield = [earlsfield_coord, "Earlsfield",12];
+
+ var stations = {"Waterloo London":waterloo_london_coord,
+                 "Clapham Junction":clapham_junction_main_coord,
+                 "Vauxhall":vauxhall_main_coord,
+                 "Wimbledon":wimbledon_coord,
+                 "Surbition":surbition_coord,
+                 "LIPHOOK":liphook_coord,
+                 "WALTON ON THAMES":walton_on_thames_coord,
+                 ""}
 
 
 
