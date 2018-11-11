c6655d36a6aef38d675519e4c1abbcaec0c3047d
AlvaroRausell <alvaro.rausellg@gmail.com>
2018-11-11 07:02:14 +0000

    frjfkjfdjfod

--- www/stationsDetails.js
+++ www/stationsDetails.js
@@ -1,5 +1,5 @@
  document.getElementById("detailsId").innerHTML = `
-        <img width="250px" height="200px" src = "stations/${currentStation}.png">
+        <img width="1000px" height="1000px" src = "img/banner.png">
             <h1><b> </b></h1>
         </img>
-        `;;
+        `;
